8a9b34df1e67847ed1721fb2ed57266df7e187c2
Rahul Katoch <93877186+Rahulkatoch99@users.noreply.github.com>
2022-03-22 11:38:36 +0530

    Update views.py

--- youtube/views.py
+++ youtube/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.contrib import messages
 from pytube import *
+from pytube.cli import on_progress
 
 # Create your views here.
 
 def home(request):
     if request.method=='POST':
         url=request.POST['url']
-        video= YouTube(url)
+        video= YouTube(url,on_progress_callback=on_progress)
         stream=video.streams.get_highest_resolution()
-        stream.download()
+        stream.download("/storage/emulated/0/Download")
         messages.info(request,'Video Download.....')
         return render(request,'home.html')
     return render(request,'home.html')
