aaff8f0cf98ad6269f823f4db36962a5d63c466e
Rahul Katoch <93877186+Rahulkatoch99@users.noreply.github.com>
2022-03-22 13:27:20 +0530

    Update urls.py

--- youtubedownloader/urls.py
+++ youtubedownloader/urls.py
@@ -14,19 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from youtube import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns=urlpatterns+static(settings.MEDIA_URL,
