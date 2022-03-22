a005ed506caad0afef9a974fe28c0ab32a8c91c0
Rahul Katoch <93877186+Rahulkatoch99@users.noreply.github.com>
2022-03-22 11:41:29 +0530

    Update settings.py

--- youtubedownloader/settings.py
+++ youtubedownloader/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR=BASE_DIR/'templates'
 SECRET_KEY = 'django-insecure-p8*vx404&f)v99_er(bpxcn6_62hwr1j7vcy=*t%to6y_0$i$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
