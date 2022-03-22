c850a06e91b5ecf7151fd334eff2d516f9590736
Rahul Katoch <93877186+Rahulkatoch99@users.noreply.github.com>
2022-03-22 14:58:43 +0530

    Update views.py

--- youtube/views.py
+++ youtube/views.py
@@ -7,26 +7,13 @@ import sys,os
 
 # Create your views here.
 
-# # Create your views here.
-# def createDirectory(name):
-#     path=sys.path[0]
-#     if not(os.path.isdir(f'{path}/{name}')):
-#         path =os.path.join(sys.path[0],name)
-#         os.mkdir(path)
-# directoyName='video'
-# createDirectory(directoyName)
-# path=sys.path[0]
+# Create your views here.
+
 def home(request):
     if request.method=='POST':
         url=request.POST['url']
         video= YouTube(url,on_progress_callback=on_progress)
         stream=video.streams.get_highest_resolution()
-
-        # vid=list(enumerate(stream))
-        # for i in vid:
-        #     print()
-        # save_path='/downloads/'
-        # ch=int(input())
         stream.download("C:/Users/Rahul/Downloads")
         # stream.download("/Downloads")
         messages.info(request,'Video Download.....')
